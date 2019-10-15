e15de4fb3bb24594b68d6c3946a6fa1721577a61
Shinya9082 <anhtuandtu1993@gmail.com>
2019-10-15 20:46:16 +0700

    Thuc hanh su dung toan tu

---
+++ Module1/BienKieuDuLieuVaToanTu/ThucHanhSuDungToanTu/getArea.js
@@ -0,0 +1,7 @@
+let width;
+let height;
+
+width= parseInt(prompt("Nhập chiều dài")) ;
+height= parseInt(prompt("Nhập chiều rộng"));
+let area = width*height;
+document.write("Diện tích là: "+area);
