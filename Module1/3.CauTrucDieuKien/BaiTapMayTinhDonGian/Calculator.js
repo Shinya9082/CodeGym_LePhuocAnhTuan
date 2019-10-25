b90151ac6fa819248e37a465a2db883c2bf29853
Shinya9082 <anhtuandtu1993@gmail.com>
2019-10-25 17:18:14 +0700

    May tinh co ban Operator Overlap done

--- Module1/3.CauTrucDieuKien/BaiTapMayTinhDonGian/Calculator.js
+++ Module1/3.CauTrucDieuKien/BaiTapMayTinhDonGian/Calculator.js
@@ -1,4 +1,5 @@
 let result=0;
+let operatorOverlap=false;
 
 function buttonNumber(name) {
     if (document.getElementById('result').value === '0') {
@@ -9,15 +10,20 @@ function buttonNumber(name) {
         document.getElementById('result').value=name;
         result = 0;
     }
+    operatorOverlap=false;
 }
 function buttonOperator(name) {
-    if(name==='='){
-        result=eval(document.getElementById('result').value);
-        document.getElementById('result').value=result;
-        console.log(result);
-        result = 0;
-        console.log(result);
-    }else
-    document.getElementById('result').value+=name;
+    if(!operatorOverlap){
+        if(name==='='){
+            result=eval(document.getElementById('result').value);
+            document.getElementById('result').value=result;
+  //          console.log(result);
+       //     result = 0;
+//            console.log(result);
+            operatorOverlap=true;
+        }else
+            document.getElementById('result').value+=name;
+        operatorOverlap=true;
+    }
 
 }
